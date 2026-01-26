4e9ace775a7480be5f19db6e0d9fec4de2eb2c72
Farouk Sabiou <sfarouk298@gmail.com>
2026-01-26 14:53:41 +0100

    Add navigation examples (01 and 02)

---
+++ week2/widgets_examples/lib/navigation/01/details_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Details')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text('Navigation simple', style: TextStyle(fontSize: 16)),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Retour'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
